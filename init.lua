3123c07673e3046176a1c84ce16850b646ab15e7
daxtonb <daxtonburgon@gmail.com>
2026-02-25 09:55:59 -1000

    [master] Fix nvim-treesitter config module name

--- init.lua
+++ init.lua
@@ -1164,7 +1164,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     -- Treesitter uses the `config` entrypoint for setup.
-    main = 'nvim-treesitter.config', -- Sets main module to use for opts
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'c_sharp' },
